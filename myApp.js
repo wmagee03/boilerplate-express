17f2e4d238e4252cef792a6a4114d5ce0f0dd036
Will Magee <wmagee03@gmail.com>
2021-02-24 17:24:07 -0500

    added assets and stuff

--- myApp.js
+++ myApp.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
+let indexPath = __dirname + './views/index.html';
+let assetsPath = __dirname + './public';
 
+express.static(assetsPath);
 
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
+});
 
 
 
